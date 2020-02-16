23f0d0c30d79b9636859c2d870383446d10cb8d4
Thomas Churchman <thomas@kepow.org>
2020-02-16 18:50:24 +0100

    Also install smbus.

--- stageastroplant0/02-kit-software/00-run-chroot.sh
+++ stageastroplant0/02-kit-software/00-run-chroot.sh
@@ -2,3 +2,4 @@
 
 su $FIRST_USER_NAME - -c 'python3 -m pip install git+https://github.com/AstroPlant/astroplant-peripheral-device-library@1.0.0b2'
 su $FIRST_USER_NAME - -c 'python3 -m pip install git+https://github.com/AstroPlant/astroplant-kit@1.0.0b4'
+su $FIRST_USER_NAME - -c 'python3 -m pip install smbus'
