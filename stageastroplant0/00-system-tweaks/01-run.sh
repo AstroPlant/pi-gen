c99e551c22aa2bc220255d9725b122ad174df514
Thomas Churchman <thomas@kepow.org>
2020-07-26 21:24:27 +0200

    Load i2c-dev kernel module at boot

--- stageastroplant0/00-system-tweaks/01-run.sh
+++ stageastroplant0/00-system-tweaks/01-run.sh
@@ -2,3 +2,4 @@
 
 sed -i "${ROOTFS_DIR}/boot/cmdline.txt" -e "s/console=ttyAMA0,[0-9]\+ //"
 sed -i "${ROOTFS_DIR}/boot/cmdline.txt" -e "s/console=serial0,[0-9]\+ //"
+echo "i2c-dev" >> "${ROOTFS_DIR}/etc/modules"
